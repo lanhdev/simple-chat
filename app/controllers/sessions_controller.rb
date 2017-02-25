8966d72955d789b80e4146cdf52aeccaa4214c51
Lanh Hoang <lanh.hoangcong@gmail.com>
2017-02-26 01:12:01 +0700

    Modify 'auth/facebook/callback' to 'auth/facebook'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,11 +26,11 @@ class SessionsController < ApplicationController
 
   def callback
     if user = User.from_omniauth(env["omniauth.auth"])
-      flash.now[:success] = 'Signed in by Facebook successfully'
-      session[:user_id] = @user.id
+      flash[:success] = 'Signed in by Facebook successfully'
+      session[:user_id] = user.id
       redirect_to root_path
     else
-      flash.now[:error] = "Error while signing in by Facebook. Let's register"
+      flash[:error] = "Error while signing in by Facebook. Let's register"
       redirect_to new_user_path
     end
   end
